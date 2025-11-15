4b28f04030025e6643ab480e0645c8f4b09cfde3
viet6cltt <phanvanviet70@gmail.com>
2025-11-15 22:51:16 +0700

    feat: add some Models for restaurant, menuitems, cart

---
+++ backend/src/models/MenuItem.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+
+const optionGroupSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true},
+    isRequired: { type: Boolean, default: false },
+    maxSelect: { type: Number, default: 1 },
+
+    options: [
+      {
+        name: { type: String, required: true },
+        price: { type: Number, default: 0 },
+      }
+    ]
+  },
+  { _id: false }
+);
+
+const menuItemSchema = new mongoose.Schema({
+  restaurantId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Restaurant",
+    required: true,
+    index: true,
+  },
+
+  categoryId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+    index: true,
+  },
+
+  name: { type: String, required: true, trim: true },
+
+  description: { type: String, default: null },
+
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+
+  imageUrl: { type: String, trim: true },
+
+  isAvailable: { type: Boolean, default: true },
+
+  rating: {
+    type: Number,
+    default: 4.5,
+    min: 0,
+    max: 5,
+  },
+
+  optionGroups: {
+    type: [optionGroupSchema],
+    default: []
+  },
+
+  totalBuy: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+menuItemSchema.index({ restaurantId: 1, categoryId: 1 });
+
+
+module.exports = mongoose.model("MenuItem", menuItemSchema);
+
---
+++ backend/src/models/Restaurant.js
@@ -0,0 +1,79 @@
+const mongoose = require('mongoose');
+
+const restaurantSchema = new mongoose.Schema({
+  name: { type: String, required: true, trim: true },
+
+    ownerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    description: { type: String, default: null },
+
+    bannerUrl: { type: String, trim: true },
+
+    address: {
+      full: { type: String, required: true }, 
+      street: { type: String },
+      ward: { type: String },
+      district: { type: String },
+      city: { type: String },
+      geo: {
+        type: {
+          type: String,
+          enum: ['Point'],
+          default: 'Point',
+        },
+        coordinates: {
+          type: [Number], // [longitude, latitude]
+          index: '2dsphere',
+        },
+      },
+    },
+
+    phone: { type: String, trim: true, required: true },
+
+    opening_time: { type: String, trim: true, default: "08:00" },
+    closing_time: { type: String, trim: true, default: "22:00" },
+
+    isActive: { type: Boolean, default: true },
+
+    rating: {
+      type: Number,
+      default: 4.5,
+      min: 0,
+      max: 5,
+    },
+
+    reviewCount: { type: Number, default: 0 },
+
+    categoriesId: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+      }
+    ],
+
+    shippingPolicy: {
+      type: String,
+      enum: ["SELP_SHIP", "PARTNER_SHIP", "HYBRID"],
+      default: "SELF_SHIP",
+    },
+
+    baseShippingFee: { type: Number, default: 0 },
+    shippingPerKm: { type: Number, default: 3000 },
+
+    estimatedDeliveryTime: { type: Number, default: 20 },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+restaurantSchema.index({ "address.geo": "2dsphere" });
+restaurantSchema.index({ ownerId: 1 });
+restaurantSchema.index({ isActive: 1 });
+restaurantSchema.index({ categoriesId: 1 });
+
+module.exports = mongoose.model("Restaurant", restaurantSchema);
